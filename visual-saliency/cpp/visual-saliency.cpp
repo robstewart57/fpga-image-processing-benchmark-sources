d6475f150d1b0074abca8523e9553f50793dcdfb
Rob Stewart <robstewart57@gmail.com>
2017-07-18 02:14:38 +0100

    WIP visual saliency c++

--- visual-saliency/cpp/visual-saliency.cpp
+++ visual-saliency/cpp/visual-saliency.cpp
@@ -10,18 +10,35 @@ using namespace std;
 #define width 512
 #define height 512
 
-/* example on Y channel */
-/*
-void zeros(uchar **image) {
-  for (size_t i = 0; i < width; ++i)
-    {
-      for (size_t j = 0; j < height; ++j) {
-        std::cout << +image[i][j] << std::endl;
-        image[i][j] = 0;
-      }
+void saveImage(string const &fname, uchar** image, int w, int h) {
+  /* convert array of pointers to consecutive memory */
+  uchar *data = new uchar[w*h];
+  for (int j=0; j<h; j++) {
+    for (int i=0; i<w; i++) {
+      data[w*j+i] = image[j][i];
     }
+  }
+
+  Mat newY = Mat(Size(w,h), CV_8UC1, data);
+  imwrite(fname,newY);
+  // Mat new_channels[3] = { newY , newY , newY };
+  // Mat new_ycrcb(Size(w,h), CV_8UC1);
+  // merge( new_channels, 3,  new_ycrcb );
+  // cvtColor(new_ycrcb, new_ycrcb, CV_YCrCb2BGR);
+  // imwrite( "tmp1.png" , new_ycrcb );
 }
-*/
+
+/* forward wavelet transform */
+void blurFilter (uchar **image, int w, int h) {
+  for (int i=1;i<w-1;++i) {
+    for (int j=1;i<h-1;++i) {
+      image[i][j] =
+        ( image[i-1][j+1] + image[i][j+1] + image[i+1][j+1]
+          + image[i-1][j] + image[i][j] + image[i+1][j]
+          + image[i-1][j-1] + image[i][j-1] + image[i+1][j-1]) / 9;
+    }
+  }
+}
 
 struct fwt_t {
   uchar **ll;
@@ -31,44 +48,201 @@ struct fwt_t {
 } fwt_bands ;
 
 /* forward wavelet transform */
-void fwt(uchar **image, int w, int h) {
+fwt_t fwt(uchar **image, int w, int h) {
   /* step 1: row wise */
-  uchar **lBand = new uchar*[w/2];
-  uchar **hBand = new uchar*[w/2];
+  uchar **lBand = new uchar*[h];
+  uchar **hBand = new uchar*[h];
+  uchar **llBand = new uchar*[h/2];
+  uchar **lhBand = new uchar*[h/2];
+  uchar **hlBand = new uchar*[h/2];
+  uchar **hhBand = new uchar*[h/2];
 
-  for (int i=0; i<w/2; ++i)
-    hBand[i] = new uchar[h];
+  for (int i=0; i<h; ++i)
+    hBand[i] = new uchar[w/2];
+  for (int i=0; i<h; ++i)
+    lBand[i] = new uchar[w/2];
 
-  for (int i=0; i<w/2; ++i)
-    lBand[i] = new uchar[h];
+  for (int row=0;row<h;row++) {
+    // lBand[row][0] = 0;
+    // lBand[row][w-1] = 0;
+    for (int col=0;col<w;++col) {
+      int left = col == 0 ? 0 : col-1;
+      int right = col == w-1 ? w-1 : col+1;
 
-  for (int row=1;row<h-1;++row) {
-    int lBand_col_count = 0;
-    int hBand_col_count = 0;
-    for (int col=1;col<w-1;++col) {
       if (col % 2 == 0) { // low
-        lBand[lBand_col_count][row] =
+        lBand[row][(int)(col/2)] =
           image[row][col]
-          + ((image[row][col-1]
-              + image[row][col+1])>>2);
-        lBand_col_count++;
+          + ((image[row][left]
+              + image[row][right])>>2);
       }
       else { // high
-        hBand[hBand_col_count][row] =
-          image[col][row]
-          - ((image[row][col-1]
-              + image[row][col+1])>>1);
-        hBand_col_count++;
+        hBand[row][(int)(col/2)] =
+          image[row][col]
+          - ((image[row][left]
+              + image[row][right])>>1);
       }
     }
   }
 
+  /* step 2: column wise low */
+  for (int i=0; i<h/2; ++i)
+    lhBand[i] = new uchar[w/2];
+  for (int i=0; i<h/2; ++i)
+    llBand[i] = new uchar[w/2];
+
+  for (int row=0;row<h;row++) {
+    for (int col=0;col<w/2;++col) {
+    // llBand[0][col] = 0;
+    // llBand[h-1][col] = 0;
+      int above = row == 0 ? 0 : row-1;
+      int below = row == h-1 ? h-1 : row+1;
+
+      if (row % 2 == 0) { // low
+        llBand[(int)row/2][col] =
+          lBand[row][col]
+          + ((lBand[above][col]
+              + lBand[below][col])>>2);
+      }
+      else { // high
+        lhBand[(int)row/2][col] =
+          lBand[row][col]
+          - ((lBand[above][col]
+              + lBand[below][col])>>1);
+      }
+    }
+  }
+
+  /* step 3: column wise high */
+  for (int i=0; i<h/2; ++i)
+    hlBand[i] = new uchar[w/2];
+  for (int i=0; i<h/2; ++i)
+    hhBand[i] = new uchar[w/2];
+
+  for (int row=0;row<h;row++) {
+    for (int col=0;col<w/2;++col) {
+      int above = row == 0 ? 0 : row-1;
+      int below = row == h-1 ? h-1 : row+1;
+
+      if (row % 2 == 0) { // low
+        hlBand[(int)row/2][col] =
+          hBand[row][col]
+          + ((hBand[above][col]
+              + hBand[below][col])>>2);
+      }
+      else { // high
+        hhBand[(int)row/2][col] =
+          hBand[row][col]
+          - ((hBand[above][col]
+              + hBand[below][col])>>1);
+      }
+    }
+  }
+
+  fwt_t bands = { llBand, lhBand, hlBand, hhBand };
+  return bands;
 }
 
-
-void visualSaliency(uchar **image) {
-  fwt(image, 512, 512);
+uchar** mapForm(uchar **l1,uchar **l2,uchar **l3,uchar **l4,uchar
+                **l5, int w, int h) {
+  uchar **newImage = new uchar*[h];
+  for (int i=0; i<h; ++i) {
+    newImage[i] = new uchar[w];
+  }
+  for (int i=0;i<w;++i) {
+    for (int j=0;j<h;++j) {
+      newImage[i][j] =
+        l1[i][j] + l2[i][j]*6 + l3[i][j]*10 + l4[i][j]*6 + l5[i][j];
+    }
+  }
+  return newImage;
 }
+
+uchar** orientForm(uchar **image1,uchar **image2,uchar **image3, int
+  w, int h) {
+  uchar **newImage = new uchar*[h];
+  for (int i=0; i<h; ++i) {
+    newImage[i] = new uchar[w];
+  }
+  for (int i=0;i<w;++i) {
+    for (int j=0;j<h;++j) {
+      newImage[i][j] =
+        image1[i][j]*2 + image2[i][j]*2 + image3[i][j]*2;
+    }
+  }
+  return newImage;
+}
+
+uchar** resize(uchar **image, int w, int h, int scale) {
+  uchar **scaledImage = new uchar*[h*scale];
+  for (int i=0; i<h*scale; ++i) {
+    scaledImage[i] = new uchar[w*scale];
+  }
+  for (int i=0; i<w; ++i) {
+    for (int j=0;j<h; ++j) {
+      for (int k=1;k<=scale;++k) {
+        scaledImage[k*i][j] = image[i][j];
+      }
+    }
+  }
+  return scaledImage;
+}
+
+uchar** visualSaliency(uchar **image) {
+  fwt_t l1 = fwt(image, 512, 512);
+  fwt_t l2 = fwt(l1.ll, 256, 256);
+  fwt_t l3 = fwt(l2.ll, 128, 128);
+  fwt_t l4 = fwt(l3.ll, 64, 64);
+  fwt_t l5 = fwt(l4.ll, 32, 32);
+
+
+  blurFilter(l1.lh,256,256);
+  blurFilter(l2.lh,128,128);
+  blurFilter(l3.lh,64,64);
+  blurFilter(l4.lh,32,32);
+  blurFilter(l5.lh,16,16);
+
+  blurFilter(l1.hl,256,256);
+  blurFilter(l2.hl,128,128);
+  blurFilter(l3.hl,64,64);
+  blurFilter(l4.hl,32,32);
+  blurFilter(l5.hl,16,16);
+
+  blurFilter(l1.hh,256,256);
+  blurFilter(l2.hh,128,128);
+  blurFilter(l3.hh,64,64);
+  blurFilter(l4.hh,32,32);
+  blurFilter(l5.hh,16,16);
+
+  uchar** lh1_resized = resize(l1.lh,256,256,2);
+  uchar** lh2_resized = resize(l2.lh,128,128,4);
+  uchar** lh3_resized = resize(l3.lh,64,64,8);
+  uchar** lh4_resized = resize(l4.lh,32,32,16);
+  uchar** lh5_resized = resize(l5.lh,16,16,32);
+
+  uchar** hl1_resized = resize(l1.hl,256,256,2);
+  uchar** hl2_resized = resize(l2.hl,128,128,4);
+  uchar** hl3_resized = resize(l3.hl,64,64,8);
+  uchar** hl4_resized = resize(l4.hl,32,32,16);
+  uchar** hl5_resized = resize(l5.hl,16,16,32);
+
+  uchar** hh1_resized = resize(l1.hh,256,256,2);
+  uchar** hh2_resized = resize(l2.hh,128,128,4);
+  uchar** hh3_resized = resize(l3.hh,64,64,8);
+  uchar** hh4_resized = resize(l4.hh,32,32,16);
+  uchar** hh5_resized = resize(l5.hh,16,16,32);
+
+  uchar** mapVer =
+  mapForm(lh1_resized,lh2_resized,lh3_resized,lh4_resized,lh5_resized,512,512);
+  uchar** mapHor =
+  mapForm(hl1_resized,hl2_resized,hl3_resized,hl4_resized,hl5_resized,512,512);
+  uchar** mapDia =
+  mapForm(hh1_resized,hh2_resized,hh3_resized,hh4_resized,hh5_resized,512,512);
+
+  uchar** mapOrient = orientForm(mapVer,mapHor,mapDia,512,512);
+
+  return mapOrient;
+}
+
 
 int main( int argc, char** argv )
 {
@@ -102,14 +276,14 @@ int main( int argc, char** argv )
 
   /* do stuff on Y channel */
 
-  visualSaliency(array);
+  uchar** result = visualSaliency(array);
 
 
   /* convert array of pointers to consecutive memory */
   uchar *data = new uchar[height*width];
   for (int i=0; i<height; ++i) {
     for (int j=0; j<width; ++j) {
-      data[i*height+j] = array[i][j];
+      data[i*height+j] = result[i][j];
     }
   }
 
